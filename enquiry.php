81a22fcaa06948380a0480c95c0051a31294138a
Adithya Krishna PN <adithyakrishnapn@protonmail.com>
2023-12-23 10:48:03 +0530

    Almost completed just the 2Fa authentication remains

---
+++ enquiry.php
@@ -0,0 +1,36 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'sendemail/phpmailer/src/Exception.php';
+require 'sendemail/phpmailer/src/PHPMailer.php';
+require 'sendemail/phpmailer/src/SMTP.php';
+
+$email = "greenwheelscs2@gmail.com";
+$sub = "Work Enquiry - GWSCS";
+
+if(isset($_POST["send"])){
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'dragoncorexgamer@gmail.com';
+    $mail->Password = 'pcgmwsgvacwfxwer';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setFrom('dragoncorexgamer@gmail.com');
+    $mail->addAddress($email);
+    $mail->isHTML(true);
+    $mail->Subject = $sub;
+    $mail->Body = "Name : ".$_POST["firm_name"]."<br> Mail : ".$_POST["mail"]."<br> Number : ".$_POST["number"]."<br> Location : ".$_POST["location"]."<br> Purpose : ".$_POST["message"];
+
+    $mail->send();
+    
+    echo "<script>
+    alert('Sent Successfully');
+    document.location.href='enquiry.html';
+    </script>";
+}
+?>
